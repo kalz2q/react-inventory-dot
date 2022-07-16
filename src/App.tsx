94263d3a614701ac19e93a5151c670e1b95aedb5
kalz2q <katsu95i@gmail.com>
2022-07-16 16:16:36 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,24 @@
 import React from "react";
 import "./App.css";
 
-type Props = {color: string}
 
-// function Counter(props: { color: string}) {
-class Counter extends React.Component {
-  constructor (props: {} | Readonly<{}>) {
-    super(props)
-  }
-  render() {
-    return (
-      <li style={{ backgroundColor: "red" }}>
-        0
-      </li>
-    );
-  }
+
+const Counter = (props: {color: string}) => {
+     return (
+       <li style={{ backgroundColor: props.color }}>
+         0
+       </li>
+     );
 }
+
 
 function App() {
   return (
     <div className="container">
       <ul>
-        <Counter  />
-        <Counter  />
-        <Counter  />
+        <Counter  color="tomato" />
+        <Counter  color="skyblue" />
+        <Counter  color="limegreen" />
       </ul>
     </div>
   );
