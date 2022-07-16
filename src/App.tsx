a5472fec00f926e45421d4fd12a64f9eb67e1ccf
kalz2q <katsu95i@gmail.com>
2022-07-16 16:59:46 +0900

    now state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "./App.css";
 
+const countUp = (color: string) => {
+  alert(color);
+}
 
-
-const Counter = (props: {color: string}) => {
-     return (
-       <li style={{ backgroundColor: props.color }}>
-         0
-       </li>
-     );
+const Counter = (props: { color: string }) => {
+  return (
+    <li style={{ backgroundColor: props.color }} onClick={() => countUp(props.color)}>
+      0
+    </li>
+  );
 }
 
 
@@ -16,9 +18,9 @@ function App() {
   return (
     <div className="container">
       <ul>
-        <Counter  color="tomato" />
-        <Counter  color="skyblue" />
-        <Counter  color="limegreen" />
+        <Counter color="tomato" />
+        <Counter color="skyblue" />
+        <Counter color="limegreen" />
       </ul>
     </div>
   );
