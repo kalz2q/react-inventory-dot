8647ab72f68eec10aeb10bbdb203552fa488c771
kalz2q <katsu95i@gmail.com>
2022-12-04 01:10:02 +0900

    M

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,13 @@ import React, { useState } from "react";
 import "./App.css";
 // rewriting react-inventory-dot
 const App = () => {
-  const Counter = (props: { color: string }) => {
-    const [count, setCount] = useState(0);
-
-    const countUp = () => setCount((prevCount) => prevCount + 1);
-
-    return (
-      <li style={{ backgroundColor: props.color }} onClick={() => countUp()}>
-        {count}
-      </li>
-    );
-  };
+  const name: string = "tom";
 
   return (
     <div className="App">
-      <div className="container">
-        <ul className="inventoryList">
-          {counters.map((counter) => (
-            <li key={counter.id}>
-              <div>
-                
-              </div>
-            </li>
-          ))}
-        </ul>
-        <div>TOTAL INVENTORY: 3</div>
-      </div>
+      <h1>hello, {name.toUpperCase()}</h1>
+      <hr />
+      <div className="box"></div>
     </div>
   );
 };
