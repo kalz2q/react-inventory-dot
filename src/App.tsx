92fd0a74d26a04057a2f4f261ffdcd50f6dc1b46
kalz2q <katsu95i@gmail.com>
2022-12-07 07:03:43 +0900

    MM

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,52 @@ import "./App.css";
 const { useState } = React;
 
 const App = () => {
-  const [songs, setSongs] = useState([
-    { title: "Song 1" },
-    { title: "Song 2" },
-    { title: "Song 3" }
+  const [songs, setSongs] = useState<Song[]>([
+    {
+      "id": 3,
+      "title": "Song 1"
+    },
+    {
+      "id": 138,
+      "title": "Song 2"
+    },
+    {
+      "id": 100,
+      "title": "Song 3"
+    }
   ]);
+  // const [songs, setSongs] = useState<Song[]>([]);
 
-  // const generateID: number = () => (Object.length)
+  type Song = {
+    "id": number,
+    "title": string
+  }
 
   const addSong = () => {
-    setSongs([...songs, { title: "New Song" }]);
+    setSongs([...songs, {
+      "id": generateid(songs),
+      "title": "New Song"
+    }]);
   };
 
+  const generateid = (jsonData: Song[]) => {
+    let newId = 0;
+    if (jsonData.length !== 0) {
+      let ids: number[] = [];
+      for (let i = 0; i < jsonData.length; i++) {
+        ids = [jsonData[i].id, ...ids]
+      };
+      ids.sort((a, b) => a - b);
+      newId = ids[ids.length - 1] + 1
+    }
+
+    return newId
+  };
   return (
     <div className="song-list">
       <ul>
         {songs.map(song => {
-          return (<li key={3}>{song.title}</li>);
+          return (<li key={song["id"]}>{song["id"]} : {song["title"]}</li>);
         })}
       </ul>
       <button onClick={addSong}>Add Song</button>
